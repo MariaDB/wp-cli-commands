dc4a9c7863f76f4f15c8543bf85f50858e5d7be8
Matt <skyminds@users.noreply.github.com>
2024-03-17 16:39:23 +0100

    typo

--- src/Catalog_Command.php
+++ src/Catalog_Command.php
@@ -1,33 +1,35 @@
 <?php
 
-class Catalog_Command extends \WP_CLI_Command {
+class Catalog_Command extends \WP_CLI_Command
+{
+    /**
+     * @var \Mariadb\CatalogsPHP\Catalog
+     */
+    protected $mariadb_connection;
 
-	/**
-	 * @var \Mariadb\CatalogsPHP\Catalog
-	 */
-	protected $mariadb_connection;
+    protected function get_mariadb_connection()
+    {
 
-	protected function get_mariadb_connection() {
+        if (! empty($this->mariadb_connection)) {
+            return $this->mariadb_connection;
+        }
 
-		if ( ! empty( $this->mariadb_connection ) ) {
-			return $this->mariadb_connection;
-		}
+        // TODO check if these constants are defined.
+        $db_host = \DB_HOST; // TODO check if the constant has a port.
+        $db_port = 3306;
+        $db_user = \DB_NAME;
+        $db_pass = \DB_PASSWORD;
 
-		// TODO check if these constants are defined.
-		$db_host = \DB_HOST; // TODO check if the constant has a port.
-		$db_port = 3306;
-		$db_user = \DB_NAME;
-		$db_pass = \DB_PASSWORD;
+        // TODO: exception handling.
+        $this->mariadb_connection = new \Mariadb\CatalogsPHP\Catalog($db_host, $db_port, $db_user, $db_pass);
+    }
 
-		// TODO: exception handling.
-		$this->mariadb_connection = new \Mariadb\CatalogsPHP\Catalog( $db_host, $db_port, $db_user, $db_pass );
-	}
-
-	/**
-	 * @when before_wp_load
-	 */
-	public function create( $args, $assoc_args ) {
-		$cataloge_name = $args[0];
-		$this->get_mariadb_connection()->create( $cataloge_name );
-	}
+    /**
+     * @when before_wp_load
+     */
+    public function create($args, $assoc_args)
+    {
+        $catalog_name = $args[0];
+        $this->get_mariadb_connection()->create($catalog_name);
+    }
 }
