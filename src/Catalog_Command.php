af2bb8c7d5723b2b962f844b05b44fd6ae9c9bd8
Jan-Willem Oostendorp <webmaster@janw.me>
2024-03-17 16:37:08 +0100

    Added create catalog name.

--- src/Catalog_Command.php
+++ src/Catalog_Command.php
@@ -2,6 +2,9 @@
 
 class Catalog_Command extends \WP_CLI_Command {
 
+	/**
+	 * @var \Mariadb\CatalogsPHP\Catalog
+	 */
 	protected $mariadb_connection;
 
 	protected function get_mariadb_connection() {
@@ -10,13 +13,13 @@ class Catalog_Command extends \WP_CLI_Command {
 			return $this->mariadb_connection;
 		}
 
+		// TODO check if these constants are defined.
 		$db_host = \DB_HOST; // TODO check if the constant has a port.
 		$db_port = 3306;
 		$db_user = \DB_NAME;
 		$db_pass = \DB_PASSWORD;
 
-        // TODO: exception handling.
-
+		// TODO: exception handling.
 		$this->mariadb_connection = new \Mariadb\CatalogsPHP\Catalog( $db_host, $db_port, $db_user, $db_pass );
 	}
 
@@ -24,8 +27,7 @@ class Catalog_Command extends \WP_CLI_Command {
 	 * @when before_wp_load
 	 */
 	public function create( $args, $assoc_args ) {
-
-        
-
+		$cataloge_name = $args[0];
+		$this->get_mariadb_connection()->create( $cataloge_name );
 	}
 }
