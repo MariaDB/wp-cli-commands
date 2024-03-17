71d75654ac1630824166dbe456488ee40234f5cd
Jan-Willem Oostendorp <webmaster@janw.me>
2024-03-17 17:22:39 +0100

    Dymmy created is-enabled

--- src/Catalog_Command.php
+++ src/Catalog_Command.php
@@ -21,6 +21,9 @@ class Catalog_Command extends \WP_CLI_Command {
 
 		// TODO: exception handling.
 		$this->mariadb_connection = new \Mariadb\CatalogsPHP\Catalog( $db_host, $db_port, $db_user, $db_pass );
+
+		// TODO: check if this is a maria DB server.
+		// TODO: are catalogs enabled.
 	}
 
 	/**
@@ -29,5 +32,10 @@ class Catalog_Command extends \WP_CLI_Command {
 	public function create( $args, $assoc_args ) {
 		$cataloge_name = $args[0];
 		$this->get_mariadb_connection()->create( $cataloge_name );
+	}
+
+	public function is_enabled( $args, $assoc_args ) {
+		$cataloge_name = $args[0];
+		$this->get_mariadb_connection()->is_enabled( $cataloge_name );
 	}
 }
